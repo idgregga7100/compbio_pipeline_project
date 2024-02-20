feec003ed8f49a3d010c4ff295540cc17fc123db
idgregga7100 <isadominica@gmail.com>
2024-02-19 18:29:26 -0600

    update retrieve_reference.py

--- 02_build_index/retrieve_reference.py
+++ 02_build_index/retrieve_reference.py
@@ -2,9 +2,30 @@
 
 from Bio import Entrez
 from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 Entrez.email='igregga@luc.edu'
-handle=Entrez.efetch(db='genome',id='NC_006273.2',rettype='fasta')
-records=list(SeqIO.parse(handle,'fasta'))
+handle=Entrez.efetch(db='nucleotide',id='NC_006273.2',rettype='genbank',retmode='text')
+record=SeqIO.read(handle,format='genbank')
 
-#need transcriptome reference, so retrieve CDS features
+#get all features of type=cds to get transcriptome sections
+cdsfeatures=[]
+for feature in record.features:
+    if feature.type=='CDS':
+        cdsfeatures.append(feature)
+
+#get sequences of each cds and protein_ids, save as seq records
+rnarecords=[]
+for cds in cdsfeatures:
+    start=cds.location.start
+    end=cds.location.end
+    sequence=record.seq[start-1:end-1]
+    sequence=sequence.transcribe()
+    proteinid=cds.qualifiers['protein_id'][0]
+    rec=SeqRecord(sequence,id=proteinid)
+    rnarecords.append(rec)
+
 #write out fasta with each cds as entry with refseq protein_id as the label
+o=open('NC_006273.2_reference.fasta','w')
+SeqIO.write(handle=o,sequences=rnarecords,format='fasta')
+o.close()

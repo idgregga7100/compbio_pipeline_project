4e89d2de18a742c317555cfbdcb976bc46197257
idgregga7100 <57689906+idgregga7100@users.noreply.github.com>
2024-02-27 22:30:59 -0600

    Update make_blast_db.sh with outdir

--- 05_other_viruses/make_blast_db.sh
+++ 05_other_viruses/make_blast_db.sh
@@ -6,9 +6,24 @@
 
 #datasets download blah blah
 #class ex: makeblastdb -in ncbi_dataset/data/genomic.fna -out coronaviridae -title coronaviridae -dbtype nucl
+while :
+do
+    case "$1" in
+      -o | --outdir)
+	        outdir=$2
+	        shift 2
+	        ;;
+      *)  # No more options
+         	shift
+	        break
+	        ;;
+     esac
+done
 
 #download Betaherpesvirinae subfamily sequences for database
+#CANNOT figure out how to get this to download into a different dir so FINE
 datasets download virus genome taxon betaherpesvirinae --refseq --include genome 
 unzip ncbi_dataset.zip
-makeblastdb -in ncbi_dataset/data/genomic.fna -out betaherpesvirinae -title betaherpesvirinae -dbtype nucl
+mv ncbi_dataset ${outdir} 
+makeblastdb -in ${outdir}/ncbi_dataset/data/genomic.fna -out ${outdir}/betaherpesvirinae -title betaherpesvirinae -dbtype nucl
 

836d3aeb2e5b450428ca6c261513666a9e7bc8ef
idgregga7100 <57689906+idgregga7100@users.noreply.github.com>
2024-02-27 22:09:31 -0600

    Update build_index.sh with outdir

--- 02_build_index/build_index.sh
+++ 02_build_index/build_index.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 #script to run kallisto
 #can hardcode the output from retrieve_reference
+#no jk adding output directory arg
+while :
+do
+    case "$1" in
+      -o | --outdir)
+	        outdir=$2
+	        shift 2
+	        ;;
+      *)  # No more options
+         	shift
+	        break
+	        ;;
+     esac
+done
 
-kallisto index -i index.idx NC_006273.2_reference.fasta
+kallisto index -i ${outdir}/index.idx ${outdir}/NC_006273.2_reference.fasta

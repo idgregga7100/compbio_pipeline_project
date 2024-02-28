c5d6f5ea52574db6f053ef9069dfeea308115708
idgregga7100 <57689906+idgregga7100@users.noreply.github.com>
2024-02-27 22:25:46 -0600

    Update get_protein_fastas.py with outdir

--- 05_other_viruses/get_protein_fastas.py
+++ 05_other_viruses/get_protein_fastas.py
@@ -1,8 +1,20 @@
 #get the protein sequences from the cds sections that had significant diffex
 #args, i think none actually
+#nope jk outdir
+import argparse
+import sys
+
+#function to parse command line arguments
+def check_arg(args=None):
+    parser.add_argument('-o','--outdir',help='output directory',required='True')
+    return parser.parse_args(args)
+
+#retrieve command line arguments and assign to variables
+args=check_arg(sys.argv[1:])
+outdir=args.outdir
 
 #read in output from step 4
-f=open('sleuth_fdr0.05.txt','r').read().rstrip().split('\n')
+f=open(outdir+'/sleuth_fdr0.05.txt','r').read().rstrip().split('\n')
 #f[0] is colnames. rest of items are one line WITH tab delim chrs between cols
 
 #get all protein_ids
@@ -19,5 +31,5 @@ Entrez.email='igregga@luc.edu'
 handle=Entrez.efetch(db='protein',id=idlist,rettype='fasta',retmode='text')
 #might not need to parsing step.......no i do actually the out file needs to be writable
 records=SeqIO.parse(handle,format='fasta')
-SeqIO.write(records,handle=open('proteins.fasta','w'),format='fasta')
+SeqIO.write(records,handle=open(outdir+'/proteins.fasta','w'),format='fasta')
 

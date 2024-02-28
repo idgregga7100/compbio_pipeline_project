cf9790af2e36e517e12760a02a7492587d056114
idgregga7100 <57689906+idgregga7100@users.noreply.github.com>
2024-02-27 22:15:30 -0600

    Update process_abundance.py with outdir

--- 03_calc_tpm/process_abundance.py
+++ 03_calc_tpm/process_abundance.py
@@ -10,17 +10,19 @@ import sys
 def check_arg(args=None):
     parser=argparse.ArgumentParser(description='differential expression pipeline')
     parser.add_argument('-s','--samples',help='SRR sample numbers',required='True')
+    parser.add_argument('-o','--outdir',help='output directory',required='True')
     return parser.parse_args(args)
 
 #retrieve command line arguments and assign to variables
 args=check_arg(sys.argv[1:])
 samples=args.samples
 samples=samples.split(',')
+outdir=args.outdir
 
 print('\t'.join(['sample','min_tpm','med_tpm','mean_tpm','max_tpm']))
 
 for sample in samples:
-    infile=sample+'/abundance.tsv'
+    infile=outdir+'/'+sample+'/abundance.tsv'
 
     inlist = open(infile,'r').read().rstrip().split('\n')
     #inlist[0] is colnames. rest of items are one line WITH tab delim chrs between cols
